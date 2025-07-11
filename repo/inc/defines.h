58a0cfe77fc3f2907a3fd5171b637252caab0946
Jean Tivan <jptivan53@gmail.com>
2025-07-11 11:48:23 +0200

    ADD:  Macros muy básicos pero esenciales

---
+++ repo/inc/defines.h
@@ -0,0 +1,24 @@
+#ifndef DEFINES_H
+# define DEFINES_H
+
+//Colors
+# define R "\033[31m"
+# define G "\033[32m"
+# define Y "\033[33m"
+# define B "\033[34m"
+# define M "\033[35m"
+# define C "\033[36m"
+# define W "\033[37m"
+# define BGR "\033[41;37m"
+# define BGC "\033[46;37m"
+# define BGG "\033[42;37m"
+# define BGY "\033[43;37m"
+# define BGM "\033[45;37m"
+# define BGB "\033[44;37m"
+# define RST "\033[0m"
+
+# ifndef PI
+#  define PI 3.14159265358979323846
+# endif
+
+#endif /* defines.h */
