93ff603fcb804a76930ef87e8614ba425519cfc7
Jean Tivan <jptivan53@gmail.com>
2025-07-14 14:54:03 +0200

    ADD: Linkeando las librerías mas básicas del proyecto

---
+++ repo/inc/cub3d.h
@@ -0,0 +1,14 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdint.h>
+# include <stdbool.h>
+# include <sys/time.h>
+# include <math.h>
+
+# include "defines.h"
+
+#endif /* cub3d.h */
