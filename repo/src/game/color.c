c7444b9ae35b63657e512b7a90687f5b0337b03a
Jean Tivan <jptivan53@gmail.com>
2025-07-31 00:17:59 +0200

    ADD: Funciones para obtener el valor de cada canal de un color

--- repo/src/game/color.c
+++ repo/src/game/color.c
@@ -6,3 +6,23 @@ int32_t	get_rgba(int32_t r, int32_t g, int32_t b, int32_t a)
 		return (0);
 	return (r << 24 | g << 16 | b << 8 | a);
 }
+
+int32_t	get_r(int32_t rgba)
+{
+	return ((rgba >> 24) & 0xFF);
+}
+
+int32_t	get_g(int32_t rgba)
+{
+	return ((rgba >> 16) & 0xFF);
+}
+
+int32_t	get_b(int32_t rgba)
+{
+	return ((rgba >> 8) & 0xFF);
+}
+
+int32_t	get_a(int32_t rgba)
+{
+	return (rgba & 0xFF);
+}
