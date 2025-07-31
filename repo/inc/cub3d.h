e220bd8c2a7488cd082a8bd432ce12367bb75c50
Jean Tivan <jptivan53@gmail.com>
2025-07-31 15:34:55 +0200

    CHANGE: Nuevos prototipos de funciones

--- repo/inc/cub3d.h
+++ repo/inc/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: jtivan-r <jtivan-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 23:28:28 by erde-la-          #+#    #+#             */
-/*   Updated: 2025/07/31 01:47:15 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2025/07/31 15:34:05 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,9 +47,13 @@ void		draw_ver_line(t_game *game, int x, int start, int end, int32_t color);
 // Game utils
 bool		init_mlx(t_game	*game);
 int			**create_map(void);
-t_player	*init_player(void);
+t_player	*init_player(t_direction init_dir);
 void		setup_game(t_game *game);
+
+// Vector utils
 void		rotate_dvector(t_dvector *vec, double rot);
+void		set_dvector(t_dvector *vec, double x, double y);
+void		set_ivector(t_ivector *vec, int x, int y);
 
 // Colors
 int32_t		get_rgba(int32_t r, int32_t g, int32_t b, int32_t a);
@@ -67,6 +71,7 @@ void 		strafe_player(t_game *game, t_key_mov key);
 // Player utils
 bool 		is_wall_collision(t_map map, double y, double x, double radius);
 void		show_player(t_player *player);
+void		set_player_dir(t_player	*player, t_direction init_dir);
 
 // Hooks
 void		setup_hooks(t_game *game);
