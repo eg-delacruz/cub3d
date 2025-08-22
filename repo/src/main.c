da6ec18e5af1be4c976369b362b79417bd32a0e0
Jean Tivan <jptivan53@gmail.com>
2025-08-22 21:18:47 +0200

    FIX: Norma

--- repo/src/main.c
+++ repo/src/main.c
@@ -6,16 +6,11 @@
 /*   By: jtivan-r <jtivan-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 22:59:02 by erde-la-          #+#    #+#             */
-/*   Updated: 2025/08/13 20:42:40 by jtivan-r         ###   ########.fr       */
+/*   Updated: 2025/08/22 21:14:39 by jtivan-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include "MLX42/MLX42.h"
-
 
 int32_t	main(int argc, char *args[])
 {
@@ -30,11 +25,6 @@ int32_t	main(int argc, char *args[])
 		return (EXIT_FAILURE);
 	if (!setup_game(game))
 		return (EXIT_FAILURE);
-	show_player(game->p);
-	int i;
-	i = -1;
-	while (++i < game->map_rows)
-		printf("%s\n", game->map[i]);
 	raycasting(game);
 	mlx_loop(game->mlx);
 	mlx_terminate(game->mlx);
